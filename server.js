52561ad5f9bc2c89ec6f41941ec6a1435c70f382
Keeley <wil11070@byui.edu>
2022-04-30 13:09:14 -0700

    video 4 help

--- server.js
+++ server.js
@@ -2,10 +2,26 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-    res.send('Spencer Imlah');
-  });
+app.use('/', require('./routes'))
 
 app.listen(port, () => {
     console.log(`App running on port ${port}`)
 })
+
+const dotenv = require('dotenv');
+dotenv.config(); 
+
+const MongoClient = require('mongodb').MongoClient;
+MongoClient.connect(process.env.MONGODB_URI, function(err, db) {
+  if (err) throw err;
+  var dbo = db.db("contacts");
+  dbo.collection("contacts").find().toArray(function(err, result) {
+    if (err) throw err;
+    // logs to console
+    console.log(result);
+    // sends to browser as json
+    //res.json(result);
+    db.close()
+  });
+});
+  
