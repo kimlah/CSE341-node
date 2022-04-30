bc57bcbf919677265d38a065a23bae7d84b7c291
Keeley <wil11070@byui.edu>
2022-04-30 15:33:16 -0700

    db code

--- routes/contacts.js
+++ routes/contacts.js
@@ -1,30 +1,13 @@
 const routes = require('express').Router();
+const connect = require('../db/connect');
 
 routes.get('/', (req, res) => {
 
-    //res.send('Hello, Contacts!');
-
-    const dotenv = require('dotenv');
-    dotenv.config(); 
-    
-    const MongoClient = require('mongodb').MongoClient;
-    MongoClient.connect(process.env.MONGODB_URI, function(err, db) {
-      if (err) throw err;
-      var dbo = db.db("contacts");
-      dbo.collection("contacts").find().toArray(function(err, result) {
+      connect.getCollection().find().toArray(function(err, result) {
         if (err) throw err;
-        // logs to console
-        //console.log(result);
-        // sends to browser as json
         res.json(result);
-        db.close()
+        console.log("Contacts Query Successful")
       });
-    });
-      
-
-
-
 });
-
 
 module.exports = routes;
